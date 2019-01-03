00706cc0380f3a9022584765827fbd8b82d8fb64
Neal Patel <nealpatel4@gmail.com>
2019-01-02 18:55:32 -0800

    Refactoring input parameters and logic

--- SouthwestCheckIn.py
+++ SouthwestCheckIn.py
@@ -1,13 +1,30 @@
+import sys, time, datetime
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-# Set Confirmation Number, First Name, and Last Name (CheckIn Parameters)
-confirmationNumber = ''
-firstName = ''
-lastName = ''
+if len(sys.argv) != 5:
+	print("ERROR: Improper commandline arguments!")
+	print("Usage: python SouthwestCheckIn.py <confirmationNumber> <firstName> <lastName> <checkInTime>")
+	exit()
 
+# Set Confirmation Number, First Name, and Last Name (CheckIn Parameters)
+confirmationNumber = sys.argv[1]
+firstName = sys.argv[2]
+lastName = sys.argv[3]
+checkInTime = sys.argv[4]
+date = datetime.datetime.strptime(checkInTime, "%m/%d/%Y-%H:%M")
+
+print(date)
+print(datetime.datetime.now().replace(hour=20))
+delta = datetime.timedelta(minutes=1)
+print(datetime.datetime.now() + delta)
+
+while datetime.datetime.now() < date:
+	sleep(1)
+
+print(datetime.datetime.now())
 driver = webdriver.Chrome('/usr/local/bin/chromedriver')
 driver.get('https://www.southwest.com/')
 
@@ -29,3 +46,4 @@ reviewResultsCheckInButton = wait.until(EC.visibility_of_element_located((By.CLA
 reviewResultsCheckInButton.click()
 
 print("Successfully checked in passenger %s %s with confirmation number %s" %(firstName, lastName, confirmationNumber))
+exit()
